4a88001145f5e533df4d82b5768a064c7be393e0
André Ramos do Vale <andre_ramosdovale@outlook.com>
2026-03-29 12:56:37 -0300

    feat: refactor security code smell

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3030',
+    origin: process.env.CORS_ORIGINS?.split(',') ?? [],
     credentials: true,
   });
 
